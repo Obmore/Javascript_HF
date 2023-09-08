d2caab6dd878c0fbbe0d88300b8415c3989b817d
Obmore <bendzsiott1998@gmail.com>
2023-09-08 16:34:41 +0200

    First javascript code

---
+++ teszt.js
@@ -0,0 +1,8 @@
+var http = require('http');
+
+http.createServer(function (request, response) {
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.end('Helló, világ!\n');
+}).listen(8000);
+
+console.log('A szerver itt fut: http://localhost:8000/');
