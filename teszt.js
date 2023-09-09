c3404f19cd51ef89b4442abab80ef50ed797e22f
Obmore <bendzsiott1998@gmail.com>
2023-09-09 20:32:51 +0200

    A házi specifikációja

--- teszt.js
+++ teszt.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 http.createServer(function (request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.end('Helló, világ!\n');
+    response.end('Hello, World!\n');
 }).listen(8000);
 
 console.log('A szerver itt fut: http://localhost:8000/');
