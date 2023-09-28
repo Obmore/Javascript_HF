6c9ecb45fb66fd1851ccbaa7182cb850824a4b2a
Obmore <139789324+Obmore@users.noreply.github.com>
2023-09-28 17:44:59 +0200

    Some edit

--- middleware/requireOption.js
+++ middleware/requireOption.js
@@ -4,6 +4,7 @@
  * @param propertyName dependency name
  * @returns {*}
  */
+
 function requireOption(objectRepository, propertyName) {
     if (objectRepository && objectRepository[propertyName]) {
         return objectRepository[propertyName];
